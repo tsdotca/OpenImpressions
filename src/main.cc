d77089100f6c2e96b1e4c2e8382d7520a1100601
Alex <alex@theshrine.ca>
2022-06-10 20:00:20 -0600

    add SDL_Image

--- src/main.cc
+++ src/main.cc
@@ -1,17 +1,23 @@
 #include <cstdio>
 
 #include "SDL.h"
+#include "SDL_image.h"
 
 const int kScreenWidth = 640;
 const int kScreenHeight = 480;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	SDL_Window *window = NULL;
 	SDL_Surface *screenSurface = NULL;
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		printf("error: SDL could not init: %s\n", SDL_GetError());
+		return 1;
+	}
+	
+	if (IMG_Init(IMG_INIT_PNG) == 0) {
+		printf("error: SDL_Image could not init: %s\n", IMG_GetError());
 		return 1;
 	}
 
@@ -21,11 +27,22 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
+	SDL_Surface* window_icon = IMG_Load("gfx/hand_logo.png");
+	if (!window_icon) {
+		printf("error: can't get window icon: %s", IMG_GetError());
+	}
+	else {
+		SDL_SetWindowIcon(window, window_icon);
+	}
+
 	screenSurface = SDL_GetWindowSurface(window);
 	SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 	SDL_UpdateWindowSurface(window);
 	SDL_Delay(2000);
 
+	if (window_icon) {
+		SDL_FreeSurface(window_icon);
+	}
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
